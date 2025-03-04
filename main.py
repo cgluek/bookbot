cc0c7c0e16e1973e1614a55b001be5c90e054a1b
cgluek <cjlgluek@gmail.com>
2025-03-03 22:57:02 -0500

    Add sys.argv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, sort_dict, get_char_count
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,16 +9,19 @@ def get_book_text(path_to_file):
         return num_words, char_count
 
 def main():
-    num_words, char_count = get_book_text('./books/frankenstein.txt')
-    char_count = sort_dict(char_count)
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f'Found {num_words} total words')
-    print("--------- Character Count -------")
-    for item in char_count:
-        if item["char"].isalpha() == True:
-            print(f"{item['char']}: {item['count']}")
-    print("============= END ===============")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+    else:
+        num_words, char_count = get_book_text(str(sys.argv[1]))
+        char_count = sort_dict(char_count)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}")
+        print("----------- Word Count ----------")
+        print(f'Found {num_words} total words')
+        print("--------- Character Count -------")
+        for item in char_count:
+            if item["char"].isalpha() == True:
+                print(f"{item['char']}: {item['count']}")
+        print("============= END ===============")
 
 main()
